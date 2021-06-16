d0e38508c37b54b8fdd714e7779c6e888dcb5618
Serge Poueme <10876571+sdpoueme@users.noreply.github.com>
2021-06-15 23:19:19 -0400

    Update aws-sla-extractor.py

--- python/aws-sla-extractor/aws-sla-extractor.py
+++ python/aws-sla-extractor/aws-sla-extractor.py
@@ -1,6 +1,6 @@
-from lxml import html
 from lxml.html import fromstring
-import yaml,html2text,requests, re, boto3, json
+import yaml,html2text,requests, boto3
+
 
 
 with open(r'sla-sources.yaml') as file:
@@ -10,37 +10,40 @@ with open(r'sla-sources.yaml') as file:
 
     #print(sources)
 
-    for source in sources:
-        #print('getting sla from '+source)
-        page = requests.get(source)
-        parser = html2text.HTML2Text()
-        parser.ignore_links = True
 
-        start = parser.handle(page.text).find("AWS will") + len("AWS will")
-        end = parser.handle(page.text).find("Service Credits") + len("Service Credits")
+for link in sources:
+    #print('getting sla from '+link)
+    page = requests.get(link)
+    parser = html2text.HTML2Text()
+    parser.ignore_links = True
 
-        sla_text = substring = parser.handle(page.text)[start:end]
-        #print(sla_text)
-        comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-        #print('Calling DetectEntities')
-        #print(json.dumps(comprehend.detect_entities(Text=sla_text, LanguageCode='en'), sort_keys=True, indent=4))
-        #print('End of DetectEntities\n')
+    start = parser.handle(page.text).find("AWS will") + len("AWS will")
+    end = parser.handle(page.text).find("Service Credits") + len("Service Credits")
 
-        sla_entities=comprehend.detect_entities(Text=sla_text.rstrip('\n'), LanguageCode='en')['Entities']
+    sla_text = substring = parser.handle(page.text)[start:end]
+    #print(sla_text)
+    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+    #print('Calling DetectEntities')
+    #print(json.dumps(comprehend.detect_entities(Text=sla_text, LanguageCode='en'), sort_keys=True, indent=4))
+    #print('End of DetectEntities\n')
 
-        #print(sla_entities)
-        service_name=fromstring(page.content).findtext('.//title')
-        service_sla=""
-        entity=""
+    sla_entities=comprehend.detect_entities(Text=sla_text.rstrip('\n'), LanguageCode='en')['Entities']
 
-        for entity in sla_entities:
+    #print(sla_entities)
+    service_name=fromstring(page.content).findtext('.//title')
+    service_sla=""
+    entity=""
 
-            if(entity['Type']=='QUANTITY' and "%" in entity['Text']):
-                service_sla=entity['Text']
-            if(service_sla!=""):
-                break
-        if (service_sla==""):
-            service_sla="SLA missing on page"
-        print(service_name+": "+service_sla)
+    for entity in sla_entities:
+
+        if(entity['Type']=='QUANTITY' and "%" in entity['Text']):
+            service_sla=entity['Text']
+        if(entity['Type']=='COMMERCIAL_ITEM' and service_name=='SLA'):
+            service_name=entity['Text']
+        if(service_sla!=""):
+            break
+    if (service_sla==""):
+        service_sla="SLA missing on page"
+    print(service_name+": "+service_sla)
 
 
